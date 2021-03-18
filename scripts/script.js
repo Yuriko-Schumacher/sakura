50861411d3921a59479fbf48fee918d2a67f44ef
Yuriko Anzai Schumacher <yuriko.schumacher@gmail.com>
2021-03-18 15:29:11 -0400

    first sketch

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 const margin = { t: 50, r: 50, b: 50, l: 50 };
 const w = document.querySelector("#kyoto1200__chart").clientWidth;
-const h = document.querySelector("#kyoto1200__chart").clientHeight;
+const h = 5000;
 
 const svg = d3
 	.select("#kyoto1200__chart")
@@ -10,13 +10,13 @@ const svg = d3
 const containerG = svg.append("g").classed("container", true);
 // .attr("transform", `translate(${margin.l}, ${margin.t})`);
 
-Promise.all([d3.csv("data/kyoto1200.csv"), d3.text("asset/copy.svg")]).then(
+Promise.all([d3.csv("data/kyoto1200.csv"), d3.text("asset/patal.svg")]).then(
 	function (datasets) {
 		// data.forEach(parseData);
 		let data = datasets[0];
 		let svgEl = datasets[1];
 
-		svg.append("defs").html(svgEl);
+		let defs = containerG.append("defs").html(svgEl);
 
 		console.log(datasets);
 		let filteredData = data.filter((d) => {
@@ -28,35 +28,73 @@ Promise.all([d3.csv("data/kyoto1200.csv"), d3.text("asset/copy.svg")]).then(
 		// create scales
 		let xScale = d3
 			.scaleLinear()
-			.domain(d3.extent(filteredData, (d) => d.date_doy))
+			.domain([85, 125])
 			.range([margin.l, w - margin.r]);
 
 		let yScale = d3
 			.scaleLinear()
-			.domain([800, 2020])
+			.domain([800, 2030])
 			.range([margin.t, h - margin.b]);
 
-		let xAxis = d3.axisBottom(xScale);
-		let xAxisG = containerG
+		let colors = [
+			"#fbf3ef",
+			"#ffdee6",
+			"#fdcbd6",
+			"#f9a9be",
+			"#eb899d",
+			"#7f2935",
+			"#ce4b5c",
+			"red",
+			"orange",
+		];
+
+		let colorScale = d3
+			.scaleOrdinal()
+			.domain(Array.prototype.fill(9))
+			.range(colors);
+
+		let xAxisBottom = d3.axisBottom(xScale);
+		let xAxisTop = d3.axisTop(xScale);
+		containerG
 			.append("g")
 			.attr("transform", `translate(0, ${h - margin.b})`)
-			.call(xAxis);
+			.call(xAxisBottom);
+		containerG
+			.append("g")
+			.attr("transform", `translate(0, ${margin.t})`)
+			.call(xAxisTop);
 
 		let yAxis = d3.axisLeft(yScale);
-		let yAxisG = containerG
+		containerG
 			.append("g")
 			.attr("transform", `translate(${margin.l}, 0)`)
 			.call(yAxis);
 
-		containerG.append(svgEl.documentElement);
+		let patalG = containerG
+			.selectAll("g.patal")
+			.data(filteredData)
+			.enter()
+			.append("g")
+			.classed("patal", true)
+			.attr(
+				"transform",
+				(d) =>
+					`translate(${xScale(d.date_doy)}, ${yScale(
+						d.year
+					)}) rotate(${Math.random() * 180})` // rotation comes with temperature
+			);
+
+		patalG
+			.append("use")
+			.attr("xlink:href", "#patal-svg")
+			.attr("fill", (d) => colorScale(d.source_type))
+			.attr("fill-opacity", 0.8);
 	}
 );
+
+function drawPatals(filteredData) {}
 
 function parseData(d) {
 	d.year = +d.year;
 	d.date_doy = +d.date_doy;
 }
-
-d3.xml("asset/hanabira.svg").then((data) => {
-	d3.select("#svg-test").node().append(data.documentElement);
-});
